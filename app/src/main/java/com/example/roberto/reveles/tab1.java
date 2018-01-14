b93341fafb2410eb9b412389122a5f782ed06fd5
Roboto63 <33013318+Roboto63@users.noreply.github.com>
2018-01-13 21:20:56 -0600

    Personalizacion rara

--- app/src/main/java/com/example/roberto/reveles/tab1.java
+++ app/src/main/java/com/example/roberto/reveles/tab1.java
@@ -1,15 +1,17 @@
 package com.example.roberto.reveles;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 /**
  * Created by Roberto on 10/01/2018.
  */
+
 
 public class tab1 extends Fragment {
     @Nullable
@@ -18,4 +20,6 @@ public class tab1 extends Fragment {
         View myview=inflater.inflate (R.layout.tab1, container,false);
         return myview;
     }
+
+
 }
