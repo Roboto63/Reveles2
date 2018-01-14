4de9437f7aec5570220284be31f46a050cb1d995
Roboto63 <33013318+Roboto63@users.noreply.github.com>
2018-01-13 20:44:10 -0600

    Personalizacion rara

--- app/src/main/java/com/example/roberto/reveles/LoginActivity.java
+++ app/src/main/java/com/example/roberto/reveles/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
         usernameEditText = ( EditText) findViewById(R.id.registrousername);
@@ -45,6 +48,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
                 }
+
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
             }
         };
