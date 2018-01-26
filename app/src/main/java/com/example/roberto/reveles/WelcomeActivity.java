4fda62a324202d73735101d2b2d2c97a0e1b42c8
Roboto63 <33013318+Roboto63@users.noreply.github.com>
2018-01-26 14:55:19 -0600

    lolis lolis!!!

---
+++ app/src/main/java/com/example/roberto/reveles/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.roberto.reveles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class WelcomeActivity extends AppCompatActivity {
+    LinearLayout l1, l2;
+    Button btnsub;
+    Animation uptodown, downtoup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        btnsub = (Button) findViewById(R.id.buttonsub);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l2 = (LinearLayout) findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+    }
+}
