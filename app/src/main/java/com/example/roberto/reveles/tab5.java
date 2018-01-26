f50ae9c1da034cf6ccaa67493425d647f75f3758
Roboto63 <33013318+Roboto63@users.noreply.github.com>
2018-01-26 01:47:41 -0600

    Aliniacion de cards fase 1

--- app/src/main/java/com/example/roberto/reveles/tab5.java
+++ app/src/main/java/com/example/roberto/reveles/tab5.java
@@ -7,16 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Roberto on 10/01/2018.
  */
 
 public class tab5 extends Fragment {
+    ImageButton salir;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myview=inflater.inflate (R.layout.tab5, container,false);
         return myview;
     }
+
 }
