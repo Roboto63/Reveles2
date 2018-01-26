ceda0ff24fb5191a8eb7a58d431cdc08dda64a10
jesus2396 <shelwin2396@gmail.com>
2018-01-25 18:36:39 -0600

    main

--- app/src/main/java/com/example/roberto/reveles/MainActivity.java
+++ app/src/main/java/com/example/roberto/reveles/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
